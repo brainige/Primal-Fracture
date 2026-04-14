0b0b2730a5bc696d7db02e23bcb1ab630d1aa1db
A <aviutsab@gmail.com>
2026-04-14 13:54:53 +0530

    fixing path issues

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-oxc';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Browser-Game/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
